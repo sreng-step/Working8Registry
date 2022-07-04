a10e3990baca86e00a447c66473e31d7c3b2a348
Nasak-Ny <nasakny7@gmail.com>
2022-07-04 10:51:57 +0700

    label2

--- Garage/Form1.Designer.cs
+++ Garage/Form1.Designer.cs
@@ -34,35 +34,36 @@
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Location = new System.Drawing.Point(13, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 21);
+            this.label1.Size = new System.Drawing.Size(212, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "Number of passengers:";
             // 
             // NbPassengers
             // 
             this.NbPassengers.AutoSize = true;
-            this.NbPassengers.Location = new System.Drawing.Point(194, 13);
+            this.NbPassengers.Location = new System.Drawing.Point(233, 9);
             this.NbPassengers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NbPassengers.Name = "NbPassengers";
-            this.NbPassengers.Size = new System.Drawing.Size(28, 21);
+            this.NbPassengers.Size = new System.Drawing.Size(34, 28);
             this.NbPassengers.TabIndex = 1;
             this.NbPassengers.Text = "12";
             // 
             // Weight
             // 
             this.Weight.AutoSize = true;
-            this.Weight.Location = new System.Drawing.Point(78, 45);
+            this.Weight.Location = new System.Drawing.Point(92, 45);
             this.Weight.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Weight.Name = "Weight";
-            this.Weight.Size = new System.Drawing.Size(46, 21);
+            this.Weight.Size = new System.Drawing.Size(56, 28);
             this.Weight.TabIndex = 3;
             this.Weight.Text = "2000";
             // 
@@ -72,15 +73,15 @@
             this.label3.Location = new System.Drawing.Point(15, 45);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 21);
+            this.label3.Size = new System.Drawing.Size(69, 28);
             this.label3.TabIndex = 2;
             this.label3.Text = "Cargo:";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(185, 41);
+            this.button1.Location = new System.Drawing.Point(198, 45);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 29);
+            this.button1.Size = new System.Drawing.Size(106, 43);
             this.button1.TabIndex = 4;
             this.button1.Text = "Entrucking";
             this.button1.UseVisualStyleBackColor = true;
@@ -88,19 +89,29 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(297, 41);
+            this.button2.Location = new System.Drawing.Point(310, 45);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 29);
+            this.button2.Size = new System.Drawing.Size(106, 43);
             this.button2.TabIndex = 5;
             this.button2.Text = "Detrucking";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(423, 87);
+            this.ClientSize = new System.Drawing.Size(442, 237);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Weight);
@@ -108,7 +119,7 @@
             this.Controls.Add(this.NbPassengers);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
@@ -125,5 +136,6 @@
         private Label label3;
         private Button button1;
         private Button button2;
+        private Label label2;
     }
 }
